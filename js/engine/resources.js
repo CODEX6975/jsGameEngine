3ffdf708a2bfcb0c629d6d03d7f2b26f807aa1ca
CODEX6975 <jayvardhan2004ch@gmail.com>
2023-12-14 10:10:10 +0000

    Background added, borders updated

--- js/engine/resources.js
+++ js/engine/resources.js
@@ -15,7 +15,7 @@ const AudioFiles = {
 Images.player.src = './resources/images/player/player.png'; // Update the image path
 
 // Set the source of the enemy image.
-Images.enemy.src = './resources/images/enemy/enemy.png'; // Update the image path
+Images.enemy.src = './resources/images/enemy/enemy2.png'; // Update the image path
 
 // Export the Images and AudioFiles objects so they can be imported and used in other modules.
 export { Images, AudioFiles };
